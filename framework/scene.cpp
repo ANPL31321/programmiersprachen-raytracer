1a0065fe05e0edde467ab8f54d74141384f8f404
ANPL31321 <avkrupiy@gmail.com>
2024-08-15 18:57:30 +0200

    Read materials and shapes, check correctness

--- framework/scene.cpp
+++ framework/scene.cpp
@@ -14,3 +14,121 @@
 #include "map"
 
 
+void Scene::loadmaterial(std::istringstream& line_as_stream) {
+    std::shared_ptr<Material> parsed_material = std::make_shared<Material>(Material{ "",{0.0f,0.f,0.0f},{0.0f,0.f,0.0f},{0.0f,0.f,0.0f},0.0f });
+    line_as_stream >> parsed_material->name_;
+
+    line_as_stream >> parsed_material->ka_.r;
+    line_as_stream >> parsed_material->ka_.g;
+    line_as_stream >> parsed_material->ka_.b;
+
+    line_as_stream >> parsed_material->kd_.r;
+    line_as_stream >> parsed_material->kd_.g;
+    line_as_stream >> parsed_material->kd_.b;
+
+    line_as_stream >> parsed_material->ks_.r;
+    line_as_stream >> parsed_material->ks_.g;
+    line_as_stream >> parsed_material->ks_.b;
+
+    line_as_stream >> parsed_material->m_;
+
+    materials_.insert(std::make_pair(parsed_material->name_, parsed_material));
+
+    std::cout << "Parsed material " <<
+        parsed_material->name_ << " "
+        << parsed_material->ka_ << " " << parsed_material->kd_ << parsed_material->ks_ << std::endl;
+}
+
+void Scene::loadshape(std::istringstream& line_as_stream) {
+    std::string token;
+    line_as_stream >> token;
+    if ("sphere" == token) {
+        std::string name;
+        std::string name_of_material;
+        glm::vec3 center;
+        float radius;
+        
+        line_as_stream >> name;
+        line_as_stream >> center.x;
+        line_as_stream >> center.y;
+        line_as_stream >> center.z;
+        line_as_stream >> radius;
+        line_as_stream >> name_of_material;
+
+        auto material = materials_.find(name_of_material);
+        if (material == materials_.end()) {
+           std::cout << "Material " << name_of_material << " not found" << std::endl;
+           return;
+        }
+        shapes_.push_back(std::make_shared<Sphere>(Sphere{ name, material->second, center, radius }));
+        shapes_.back()->print(std::cout);
+    }
+    else if ("box" == token) {
+        std::string name;
+        std::string name_of_material;
+        glm::vec3 min;
+        glm::vec3 max;
+
+        line_as_stream >> name;
+        line_as_stream >> min.x;
+        line_as_stream >> min.y;
+        line_as_stream >> min.z;
+        line_as_stream >> max.x;
+        line_as_stream >> max.y;
+        line_as_stream >> max.z;
+        line_as_stream >> name_of_material;
+
+        auto material = materials_.find(name_of_material);
+        if (material == materials_.end()) {
+            std::cout << "Material " << name_of_material << " not found" << std::endl;
+            return;
+        }
+
+        shapes_.push_back(std::make_shared<Box>(Box{ name, material->second, min, max }));
+        shapes_.back()->print(std::cout);
+    }
+    else {
+        std::cout << "Unexpected keyword: " << token << std::endl;
+    }
+}
+
+void Scene::loadscene() {
+    std::ifstream sdf_file(file_name_);
+    if (!sdf_file.is_open()) {
+        std::cout << "Could not find or open: " << file_name_ << std::endl;
+        return;
+    }
+    std::string line_buffer;
+    while (std::getline(sdf_file, line_buffer)) {
+        std::istringstream line_as_stream(line_buffer);
+        std::string token;
+        line_as_stream >> token;
+        if ("define" == token) {
+            line_as_stream >> token;
+            if ("material" == token) {
+                loadmaterial(line_as_stream);
+            }
+            else if ("shape" == token) {
+                loadshape(line_as_stream);
+            }
+            else {
+                std::cout << "Unexpected keyword: " << token << std::endl;
+            }
+        }
+        else {
+            std::cout << "Unexpected keyword: " << token << std::endl;
+        }
+
+    }
+
+    sdf_file.close();
+}
+
+Scene::Scene(int x_res, int y_res, std::string const& file_name, Camera const& camera, Color const& ambient):
+	x_res_{ x_res },
+	y_res_{ y_res },
+	file_name_{file_name},
+	camera_{ camera },
+	ambient_{ ambient } {
+    loadscene();
+}
--- framework/scene.hpp
+++ framework/scene.hpp
@@ -4,18 +4,34 @@
 
 #include <string>
 #include "shape.hpp"
-#include "vector"
-#include "map"
+#include <vector>
+#include <map>
+#include "camera.hpp"
+#include "color.hpp"
+#include "punktlichtquelle.hpp"
+#include "sphere.hpp"
+#include "box.hpp"
 
 #ifndef RAYTRACER_SCENE_HPP
 #define RAYTRACER_SCENE_HPP
 
-struct Scene{
+class Scene{
+public:
+    Scene(int x_res, int y_res, std::string const& file_name, Camera const& camera, Color const& ambient);
+    // public only for tests
+    std::vector<std::shared_ptr<Shape>> shapes_;
+    std::map<std::string, std::shared_ptr<Material>> materials_;
+private:
+    void loadscene();
+    void loadmaterial(std::istringstream& line_as_stream);
+    void loadshape(std::istringstream& line_as_stream);
     int x_res_, y_res_;
     std::string file_name_;
-    std::vector<Shape> shapes_;
-    std::map<std::string ,std::shared_ptr<Material>> materials_;
-
+    //std::vector<std::shared_ptr<Shape>> shapes_;
+    //std::map<std::string ,std::shared_ptr<Material>> materials_;
+    Camera camera_;
+    Color ambient_;
+    std::vector<Punktlichquelle> punktlichtquellen_;
 };
 
 
