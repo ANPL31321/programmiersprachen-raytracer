1bc6bad3cba14c4ac0be94345c7ba5e77f02ad6e
PlatypusFromCode <apolinary741@gmail.com>
2024-09-02 13:54:29 +0300

    now camera can move

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -15,11 +15,10 @@ Ray Camera::transform_camera_rays(Ray const& ray) const {
     glm::mat4 mat4{
             glm::vec4(right, 0.0f),
             glm::vec4(up, 0.0f),
-            glm::vec4(-forward, 0.0f), // Negative direction for right-handed coordinate system
-            glm::vec4(eye, 1.0f) // Translate camera to origin
+            glm::vec4(-forward, 0.0f),
+            glm::vec4(eye, 1.0f)
     };
 
-    // Apply the transformation matrix
     glm::vec4 origin_in_HCS{ray.origin, 1.0f};
     glm::vec4 direction_in_HCS{ray.direction, 0.0f};
 
